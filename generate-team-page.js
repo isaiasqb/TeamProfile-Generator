025b04c798feccaf3d563b9ca6f05b840cb1bcea
IsaiasQB <isaias.qb@hotmail.com>
2022-04-29 18:43:11 -0400

    added fs functions to copy and generate css and html files

---
+++ generate-team-page.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const team ="this is my team"
+
+  // function for creating the html site
+const writeHtmlFile = content => {
+  fs.writeFile('./dist/testpage.html', team, err => {
+    if(err) {
+      console.log('Opps something happened!')
+      return
+    } 
+    console.log('Team website was generated in distribution')
+  })
+}
+
+
+  // fucntion to copi file for the css template
+const copyCssFile = () => {
+  fs.copyFile('./src/style.css', './dist/teststyle.css', err => {
+    if(err) {
+      console.log('Opps something happened!')
+      return
+    } 
+    console.log('CSS file has been copied onto distribution')
+  })
+}
+
+  module.exports = {writeHtmlFile, copyCssFile}
---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const { copyCssFile, writeHtmlFile } = require('./generate-team-page');
+
+
+writeHtmlFile()
+copyCssFile()
