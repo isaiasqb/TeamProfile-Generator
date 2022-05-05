03ae9b3bcb7a16e1b2b27adf12512765a8d3d9c6
IsaiasQB <isaias.qb@hotmail.com>
2022-05-05 01:40:15 -0400

    added questions for the prompt

--- index.js
+++ index.js
@@ -1,6 +1,158 @@
 const inquirer = require('inquirer');
 const { copyCssFile, writeHtmlFile } = require('./generate-team-page');
 
+//ask general questions about the employee
+const promptQuestions = (employees) => {
+
+       //if there is no array create one
+       if (!employees){
+        employees = [];
+    }
+
+  return inquirer.prompt([
+    {   
+      type: 'input',
+      name: 'name',
+      message: 'What is the employee name? (Required)',
+      validate: nameInput => {
+          if(nameInput){
+              return true;
+          } else {
+              console.log('Please Enter a Name!');
+              return false;
+          }
+      }
+    },
+    {   
+      type: 'list',
+      name: 'role',
+      message: 'What is the role of the employee? (Required)',
+      choices: ['Manager', 'Engineer', 'Intern'],
+      validate: roleInput => {
+          if(roleInput){
+              return true;
+          } else {
+              console.log('Please enter a role!');
+              return false;
+          }
+      }
+    },
+    {   
+      type: 'input',
+      name: 'id',
+      message: 'What is the employee ID? (Required)',
+      validate: idInput => {
+        if(idInput){
+            return true;
+        } else {
+            console.log('Please enter an ID!');
+            return false;
+        }
+      }
+    },
+    {   
+      type: 'input',
+      name: 'email',
+      message: "What is the employee's e-mail? (Required)",
+      validate: emailInput => {
+          if(emailInput){
+              return true;
+          } else {
+              console.log('Please enter an ID!');
+              return false;
+          }
+      }
+    },
+    {
+      type: 'list',
+      name: 'office',
+      message: 'What is the name office number?',
+      choices: ['0012', '0015', '0017', '0019', '0021'],
+      when: ({ role }) => role === 'Manager',
+      validate: officeInput => {
+          if(officeInput){
+              return true;
+          } else {
+              console.log('Please enter choose an office number!')
+              return false
+          }
+      }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is their github username?',
+        when: ({ role }) => role === 'Engineer',
+        validate: githubInput => {
+            if(githubInput){
+                return true;
+            } else {
+                console.log('Please enter a username!')
+                return false
+            }
+        }
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'What is the name of their school?',
+      when: ({ role }) => role === 'Intern',
+      validate: schoolInput => {
+          if(schoolInput){
+              return true;
+          } else {
+              console.log('Please enter their school name!')
+              return false
+          }
+      }
+  },
+  {
+    type: 'confirm',
+    name: 'confirmNext',
+    message: 'Would you like to add another colleague?',
+    default: false
+},
+  ]).then(employeeData =>{
+    employees.push(employeeData);
+
+    if (employeeData.confirmNext){
+        return promptQuestions(employees) //this calls the function again and prompts the project questions
+    } else {
+        return employees;
+    }
+});//end of inquirer prompt
+};
+
+
+
+
+
+
+
+
+promptQuestions()
+    // .then(promptExtraInfo)
+//     .then(teamInfo => {
+//         return generatePage(portfolioData)
+//     })
+
+//     .then(pageHTML => {
+//         return writeFile(pageHTML);
+//     })
+
+//     .then(writeFileResponse => {
+//         console.log(writeFileResponse);
+//         return copyFile();
+//     })
+
+//     .then(copyFileResponse => {
+//         console.log(copyFileResponse);
+//     })
+
+//     .catch(err => {
+//         console.log(err);
+//     })
+
 
 writeHtmlFile()
 copyCssFile()
