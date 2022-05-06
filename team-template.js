002c9721bab2e50aa3be56202286f44276c9670a
IsaiasQB <isaias.qb@hotmail.com>
2022-05-06 01:19:35 -0400

    working on displaying selected info based on role

--- team-template.js
+++ team-template.js
@@ -9,13 +9,42 @@
     }
   }
 
-const infoByRole = role =>  {   
-  if (role === 'Engineer'){
-    return '🚧';
-  } else if (role === 'Manager'){
-  return '💼';
+const officeInfo = info =>  {   
+  if (info){
+    return `
+    <div>
+    <h3>Office No:</h3>
+    <p>${info}</p>
+    </div>
+    `;
   } else {
-  return '📓';
+    return '';
+  }
+}
+
+const githubInfo = info =>  {   
+  if (info){
+    return `
+    <div>
+    <h3>Github Profile:</h3>
+    <p><a href="https://github.com/${info}" target="_blank">${info}</a></p>
+    </div>
+    `;
+  } else {
+    return '';
+  }
+}
+
+const schoolInfo = info =>  {   
+  if (info){
+    return `
+    <div>
+    <h3>School Name:</h3>
+    <p>${info}</p>
+    </div>
+    `;
+  } else {
+    return '';
   }
 }
 
@@ -24,33 +53,38 @@ const infoByRole = role =>  {
 module.exports = teamData => {
 
   return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Team Builder</title>
-    <link rel="stylesheet" href="teststyle.css">
-  </head>
-
+<!DOCTYPE html> 
+<html lang="en"> 
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Team Builder</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="teststyle.css">
+</head>
   <body>
-    <header class="container flex-row justify-space-between align-center py-3">
-              <h1 class="page-title text-secondary bg-dark py-2 px-3">MY TEAM</h1>
+    <header class="">
+      <h1 class="">My Team</h1>
     </header>
 
-    <main class="container my-5">
-    ${teamData.map(({name, role, id, email}) => {
-          return `
-        <div>
-          ${name}
-          ${generateLogo(role)} ${role}
-          ${id}
-          ${email}
-        </div>
-          `;
-      })}
+    <main class="d-flex justify-content-center">
+          ${teamData.map(({name, role, id, email, office, github, school}) => {
+                return `
+              <div>
+                <h2>${name}</h2>
+                <h3>${generateLogo(role)} ${role}</h3>
+                <p>Employee ID: ${id}</p>
+                <p>Employee Email: ${email}</p>
+
+                ${officeInfo(office)}
+                ${githubInfo(github)}
+                ${schoolInfo(school)}
+
+              </div>
+                `;
+            })
+          }
     </main>
   </body>
   </html>
